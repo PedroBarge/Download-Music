41d16d131bed3f31616c9813751157eb69fe17ed
Barge <pedro525tavares@gmail.com>
2026-08-20 19:54:54 +0100

    update some vars

--- source/apple_music/load_apple_music.py
+++ source/apple_music/load_apple_music.py
@@ -2,8 +2,8 @@ from source.apple_music.extract_apple_music_embed import extract_apple_music
 
 
 def main():
-    # user_response_name = input("Copy playlist Name: ")
-    # user_response_id = input("Copy playlist ID: ")
-    pl_name = "techno-rave" #  mock temporário
-    pl_id = "mJy8gyJsNo1gy7y" #  mock temporário
-    extract_apple_music(pl_name, pl_id)
+    # user_response_playlist_name = input("Copy playlist Name: ")
+    # user_response_plalist_id = input("Copy playlist ID: ")
+    user_response_playlist_name = "techno-rave" #  mock temporário
+    user_response_plalist_id = "mJy8gyJsNo1gy7y" #  mock temporário
+    extract_apple_music(user_response_playlist_name, user_response_plalist_id)
--- source/spotify/load_spotify.py
+++ source/spotify/load_spotify.py
@@ -3,6 +3,6 @@ from source.spotify.extract_spotify_embed import extract_spotify
 
 
 def main():
-    # user_response = input("Copy playlist ID: ")
-    user_response = "5DutDksCDPMirP40odZ0gf" # mock temporário
-    extract_spotify(user_response)
+    # user_response_playlist_id = input("Copy playlist ID: ")
+    user_response_playlist_id = "5DutDksCDPMirP40odZ0gf" # mock temporário
+    extract_spotify(user_response_playlist_id)
